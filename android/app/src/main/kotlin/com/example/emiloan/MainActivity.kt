bc115e162b95e80dfdd5c483d19db3bba1450e60
Asadbek <57528599+Asadbek-IP@users.noreply.github.com>
2024-10-11 18:13:03 +0500

    first commit

---
+++ android/app/src/main/kotlin/com/example/emiloan/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.emiloan
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ lib/main.dart
@@ -0,0 +1,81 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/material.dart';
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: true,
+      ),
+      home: const MyHomePage(title: 'Credits List'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key, required this.title});
+
+  final String title;
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  Future<List<Map<String, dynamic>>> fetchCredits() async {
+    CollectionReference credits =
+        FirebaseFirestore.instance.collection('credits');
+    QuerySnapshot snapshot = await credits.get();
+
+    return snapshot.docs
+        .map((doc) => doc.data() as Map<String, dynamic>)
+        .toList();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+        title: Text(widget.title),
+      ),
+      body: FutureBuilder<List<Map<String, dynamic>>>(
+        future: fetchCredits(),
+        builder: (context, snapshot) {
+          if (snapshot.connectionState == ConnectionState.waiting) {
+            return const Center(child: CircularProgressIndicator());
+          } else if (snapshot.hasError) {
+            return Center(child: Text('Error: ${snapshot.error}'));
+          } else if (!snapshot.hasData || snapshot.data!.isEmpty) {
+            return const Center(child: Text('No credits found.'));
+          }
+
+          List<Map<String, dynamic>> creditsList = snapshot.data!;
+
+          return ListView.builder(
+            itemCount: creditsList.length,
+            itemBuilder: (context, index) {
+              var credit = creditsList[index];
+              return ListTile(
+                title:
+                    Text(credit.toString()), // Display each credit as a string
+              );
+            },
+          );
+        },
+      ),
+    );
+  }
+}
